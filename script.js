d58c7ca3d5e5712fb36c6559c52c34f49ba1b524
KotiMango <rons20000@gmail.com>
2021-06-22 16:19:56 +0300

    Changed output to a pretty parse json string

--- script.js
+++ script.js
@@ -1,40 +1,4 @@
 const palettesGrabber = () => {
-  (function (console) {
-    console.save = function (data, filename) {
-      if (!data) {
-        console.error("Console.save: No data");
-        return;
-      }
-      if (!filename) filename = "console.json";
-      if (typeof data === "object") {
-        data = JSON.stringify(data, undefined, 4);
-      }
-      var blob = new Blob([data], { type: "text/json" }),
-        e = document.createEvent("MouseEvents"),
-        a = document.createElement("a");
-      a.download = filename;
-      a.href = window.URL.createObjectURL(blob);
-      a.dataset.downloadurl = ["text/json", a.download, a.href].join(":");
-      e.initMouseEvent(
-        "click",
-        true,
-        false,
-        window,
-        0,
-        0,
-        0,
-        0,
-        0,
-        false,
-        false,
-        false,
-        false,
-        0,
-        null
-      );
-      a.dispatchEvent(e);
-    };
-  })(console);
   const rgbToHex = (r, g, b) => {
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
   };
@@ -85,5 +49,5 @@ const palettesGrabber = () => {
       exitPalette();
     }
   }
-  console.save(paletteTitleAndSwatches, ["colors.json"]);
+  console.info(JSON.stringify(paletteTitleAndSwatches, null, " "));
 };
