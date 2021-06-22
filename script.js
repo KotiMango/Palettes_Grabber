71106d97cc66cb6c6fe3b2dcacbbb0a2ea27e509
KotiMango <rons20000@gmail.com>
2021-06-22 13:29:08 +0300

    Function wrap, refactor and interchangeable return

--- script.js
+++ script.js
@@ -1,62 +1,55 @@
-const rgbToHex = (r, g, b) => {
-  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-};
-
-const exitPalette = () => {
-  try {
-    const exitButton = document.getElementsByClassName(
-      "OneUp__closeButton___3dXkc"
-    );
-    exitButton[0].click();
-  } catch (err) {
-    console.log("Can't exit the page.");
-    console.log(err);
-  }
-};
-
-const getPaletteName = () => {
-  const paletteTextObjLst = document.getElementsByClassName(
-    "ImageAndInfoContainer__projectName___27FKf"
-  );
-  const paletteTextObj = paletteTextObjLst[0];
-  return paletteTextObj.textContent;
-};
-
-const palettes = document.getElementsByClassName("Theme__theme___2NcED");
-const paletteTitleAndSwatches = [];
-for (let i = 0; i < 50; i++) {
-  try {
-    const palette = palettes[i];
-    palette.click();
-    const paletteName = getPaletteName();
-
-    const paletteColors = [];
-    for (const chldnde of palette.childNodes) {
-      const backgroundInRgb = chldnde.style.background.split(" ").join("");
-      const rgbVals = backgroundInRgb
-        .substring(
-          backgroundInRgb.indexOf("(") + 1,
-          backgroundInRgb.indexOf(")")
-        )
-        .split(",");
-      paletteColors.push(
-        rgbToHex(
-          parseInt(rgbVals[0]),
-          parseInt(rgbVals[1]),
-          parseInt(rgbVals[2])
-        ).toUpperCase()
+const palettesGrabber = () => {
+  const rgbToHex = (r, g, b) => {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  };
+  const exitPalette = () => {
+    try {
+      const exitButton = document.getElementsByClassName(
+        "OneUp__closeButton___3dXkc"
       );
+      exitButton[0].click();
+    } catch (err) {
+      console.table([[err], ["Can't exit palette"]]);
     }
-    paletteTitleAndSwatches.push({
-      title: paletteName,
-      swatches: paletteColors,
-    });
-  } catch (err) {
-    console.log("Can't parse obj, err:");
-    console.log(err);
-  } finally {
-    exitPalette();
-  }
-}
+  };
+  const getPaletteName = () => {
+    const paletteTextObjLst = document.getElementsByClassName(
+      "ImageAndInfoContainer__projectName___27FKf"
+    );
+    return paletteTextObjLst[0].textContent;
+  };
+  const palettes = document.getElementsByClassName("Theme__theme___2NcED");
+  const paletteTitleAndSwatches = [];
+  for (let i = 0; i < 50; i++) {
+    try {
+      const palette = palettes[i];
+      palette.click();
+      const paletteName = getPaletteName();
+      const paletteColors = [];
 
-console.log(paletteTitleAndSwatches);
+      for (const chldnde of palette.childNodes) {
+        const backgroundInRgb = chldnde.style.background.split(" ").join("");
+        const rgbVals = backgroundInRgb
+          .substring(
+            backgroundInRgb.indexOf("(") + 1,
+            backgroundInRgb.indexOf(")")
+          )
+          .split(",")
+          .map(Number);
+        paletteColors.push(
+          rgbToHex(rgbVals[0], rgbVals[1], rgbVals[2]).toUpperCase()
+        );
+      }
+      paletteTitleAndSwatches.push({
+        title: paletteName,
+        swatches: paletteColors,
+      });
+    } catch (err) {
+      console.table([[err], ["Can't parse obj"]]);
+    } finally {
+      exitPalette();
+    }
+  }
+  return paletteTitleAndSwatches;
+  //return JSON.stringify(paletteTitleAndSwatches)
+};
